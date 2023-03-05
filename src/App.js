cbbf5271d5609eb49c3d4ee0c1a9f2f58570a12b
Amirkhan Salan <68302923+n7wt@users.noreply.github.com>
2023-03-06 01:53:37 +0300

    react practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./css/App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //1
+  const array = [22, 0, -45, 1001, 8, -6, -0.1, 101, 2.5, -55555];
+
+  const positive = array.filter((num) => num >= 0);
+
+  console.log(positive);
+
+  //2
+  const messages = [
+    { message: "hello", error: true },
+    { message: "javascript", error: false },
+    { message: "expovision", error: true },
+    { message: "react", error: true },
+    { message: "angular", error: false },
+    { message: "es6", error: false },
+  ];
+
+  const filteredMsgs = messages.filter((item) => {
+    return item.error === false;
+  });
+  console.log(filteredMsgs);
+
+  //3
+  const words = ["экспо", "js", "react", "css", "angular", "vue", "html"];
+
+  const newWords = words.map((word) => (word.length < 5 ? "*" : word));
+
+  console.log(newWords);
+
+  //4
+  const sendMessage = (message) => {
+    /*
+     * тут какой-то большой код для отправки сообщения
+     */
+  };
+
+  const half = (number) => {
+    return number / 2;
+  };
+
+  const showConsole = () => {
+    console.log("Экспо");
+  };
+
+  const concatWords = (first, second) => {
+    return first + second;
+  };
+
+  //5
+  function justText() {
+    return "expo";
+  }
+
+  function logging(text) {
+    console.log(text);
+  }
+
+  function add(x, y) {
+    const z = 3;
+    return z * x * y;
+  }
+
+  function onlyPositive(number) {
+    if (number < 0) {
+      return false;
+    }
+    return true;
+  }
 }
 
 export default App;
